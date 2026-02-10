30e9cea0c2bd7c14c21433b87ff02324ffbcbd75
Anish George V <54798617+ageorgev@users.noreply.github.com>
2026-02-10 18:44:32 +0530

    Add StatementProcessResponse model class

---
+++ expanalysis/src/main/java/com/agv/expenses/service/model/StatementProcessResponse.java
@@ -0,0 +1,15 @@
+package com.agv.expenses.service.model;
+
+import java.io.Serializable;
+
+@lombok.Data
+@lombok.Builder
+@lombok.NoArgsConstructor
+@lombok.AllArgsConstructor
+public class StatementProcessResponse implements Serializable {
+
+    private String status;
+    private String errorMessage;
+    private String exceptionStack;
+    
+}
